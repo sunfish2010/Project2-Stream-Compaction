dded0041c2c3ca46cbca138c11c17d1083f2a6c4
Yu Sun <sun@vagelos-ve502-0860.apn.wlan.upenn.edu>
2018-09-11 19:03:45 -0400

    naive scan completed on mac

--- stream_compaction/naive.cu
+++ stream_compaction/naive.cu
@@ -3,6 +3,8 @@
 #include "common.h"
 #include "naive.h"
 
+# define blockSize 128
+
 namespace StreamCompaction {
     namespace Naive {
         using StreamCompaction::Common::PerformanceTimer;
@@ -11,15 +13,59 @@ namespace StreamCompaction {
             static PerformanceTimer timer;
             return timer;
         }
-        // TODO: __global__
+
+        __global__ void kernNaiveScan(int N, int *odata, int *idata, int d){
+            int index = threadIdx.x + (blockIdx.x * blockDim.x);
+            if (index < N){
+                    if (index > (1 << (d - 1))) odata[index] = idata[index - (1 << (d - 1))] + idata[index];
+                    else odata[index] = idata[index];
+                }
+        }
+
+        // couldn't figure out a way to exclusive scan at once
+        __global__ void kernInclusiveToExclusive(int N, int *odata, int idata){
+            int index = threadIdx.x + (blockIdx.x * blockDim.x);
+            if (index < N){
+                if (index == 0) odata[index] = 0;
+                else odata[index] = idata[index - 1];
+            }
+        }
 
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
         void scan(int n, int *odata, const int *idata) {
+            dim3 fullBlockPerGrid((n + blockSize - 1) / blockSize);
+            int* dev_in, dev_out;
+
+            cudaMalloc((void**) &dev_in, n * sizeof(int));
+            checkCUDAError("cudaMalloc dev_in failed");
+
+            cudaMalloc((void**) &dev_out, n * sizeof(int));
+            checkCUDAError("cudaMalloc dev_out failed");
+
+            cudaMemcpy(dev_in, idata, n * sizeof(int), cudaMemcpyHostToDevice);
+            checkCUDAError("cudaMemcpy HostToDevice failed");
+
             timer().startGpuTimer();
-            // TODO
+
+            for (int d = 0; d <= ilog2ceil(n); d++) {
+                // ping-pong the buffer for 'inplace' matrix manipulation
+                kernNaiveScan <<< fullBlockPerGrid, blockSize >>> (n, dev_out, dev_in, d);
+                checkCUDAError("kernNaiveScan dev_in failed");
+                std::swap(dev_in, dev_out);
+            }
+            // result now in dev_in
+            kernInclusiveToExclusive<<<fullBlockPerGrid, blockSize>>> (n, dev_out, dev_in);
+
             timer().endGpuTimer();
+
+            cudaMemcpy(odata, dev_out, n * sizeof(int), cudaMemcpyDeviceToHost);
+            checkCUDAError("cudaMemcpy DeviceToHost failed");
+
+            cudaFree(dev_in);
+            cudaFree(dev_out);
+
         }
     }
 }
